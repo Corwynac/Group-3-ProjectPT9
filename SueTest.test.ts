19df76e39c65d6d3d2ea94538e18ceb748b1c036
Susan Gross <susanlynngross@gmail.com>
2022-11-29 16:57:56 -0500

    Sues attempt 4

--- SueTest.test.ts
+++ SueTest.test.ts
@@ -1,3 +1,4 @@
+import { Driver } from "selenium-webdriver/chrome";
 import { suePage } from "./suePageObject";
 
 const Sue = new suePage()
@@ -9,6 +10,12 @@ describe("Sues Part", () => {
 
     test("Chat Box", async() => {
         await Sue.click(Sue.chatBox)
-        
+        await Sue.setInput(Sue.nameInput, "Snow White")
+        await Sue.setInput(Sue.emailInput, "snowwhite8675@yahoo.com")
+        await Sue.setInput(Sue.helpInput, "testing, testing 123")
+        await Sue.click(Sue.signUpBox)
+        await Sue.click(Sue.sendBtn)
+        await Sue.driver.quit()
     })
+
 })
