e06d1380ba06feae343adeaf8c6c13a623c5f06a
unknown <connorgreenland05@gmail.com>
2022-11-30 19:01:59 -0700

    first push

--- connors_part/conPageObject.ts
+++ connors_part/conPageObject.ts
@@ -3,8 +3,15 @@ import { BasePage } from "../basePage";
 
 export class Connor extends BasePage {
     popUp: By = By.xpath('//div[@class="x-el x-el-div c1-1 c1-2 c1-17 c1-3n c1-4u c1-4v c1-3p c1-v c1-4w c1-d1 c1-2y c1-b c1-c c1-d c1-e c1-f c1-g"]')
-    dropDown: By = By.xpath('//select[@id="tCounty"]')
+    dropDown: By = By.xpath('//*[@class="form-control dropdown"]')
     Worcestershire: By = By.xpath('//option[@value="SC"]')
+    FbLink: By = By.xpath('//a[@href="https://www.facebook.com/groups/1960389827507491/"]')
+    TwLink: By = By.xpath('//a[@href="https://twitter.com/DarkArtsWizard"]')
+    InstLink: By =By.xpath('//a[@href="https://www.instagram.com/qtpmgrossman"]')
+    PinLink: By = By.xpath('//a[@href="https://www.pinterest.com/paulgrossman/candymapper/"]')
+    InLink: By = By.xpath('//a[@href="https://www.linkedin.com/qtpmgrossman"]')
+    YtLink: By = By.xpath('//a[@href="https://www.youtube.com/PaulGrossmanTheDarkArtsWizard"]')
+
     constructor() {
         super({url: "https://candymapperr2.com/"})
     }
--- connors_part/conTest.test.ts
+++ connors_part/conTest.test.ts
@@ -1,3 +1,4 @@
+import { Driver } from "selenium-webdriver/chrome";
 import { Connor } from "./conPageObject";
 
 const connor = new Connor()
@@ -5,8 +6,26 @@ const connor = new Connor()
 test("Close that popup", async ()=> {
     await connor.closePop()
 })
-test("Drop Down Test", async ()=> {
-    await connor.driver.executeScript("document.getElementById('iframe-undefined6').scrollIntoView(true);");
-    await connor.click(connor.dropDown)
-    await connor.click(connor.Worcestershire)
+test("Go to Facebook page", async ()=> {
+    await connor.click(connor.FbLink)
 })
+test("Go to Twitter page", async ()=>{
+    await connor.click(connor.TwLink)
+})
+test("Go to Instagram page", async ()=> {
+    await connor.click(connor.InstLink)
+})
+test("Go to Pinterest page", async ()=> {
+    await connor.click(connor.PinLink)
+})
+test("Go to LinkedIn page", async ()=> {
+    await connor.click(connor.InLink)
+})
+test("Go to Youtube Page", async ()=> {
+    await connor.click(connor.YtLink)
+    await connor.driver.quit()
+})
+//test("Drop Down Test", async ()=> {
+//    await connor.click(connor.dropDown)
+//   await connor.click(connor.Worcestershire)
+//})
