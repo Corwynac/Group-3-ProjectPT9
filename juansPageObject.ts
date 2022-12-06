6ffd1f4b0d1a280688365eb493955be4c6d9ef3c
Corwyn <corwynac@gmail.com>
2022-12-05 19:31:16 -0600

    Juans Test

--- juansPageObject.ts
+++ juansPageObject.ts
@@ -1,14 +1,15 @@
 import { By } from "selenium-webdriver";
 import { BasePage } from "./basePage";
 
-export class candyMapPage extends BasePage {
-    popUp: By = By.xpath('//div[@class="x-el x-el-div c1-1 c1-2 c1-17 c1-3n c1-4u c1-4v c1-3p c1-v c1-4w c1-d1 c1-2y c1-b c1-c c1-d c1-e c1-f c1-g"]')
-    demoLink: By = By.xpath('//a[@href="https://www.youtube.com/watch?v=3aqELBZ0Qvw"]')
-    testProjectLink: By = By.xpath('//a[@href="http://www.testproject.io/"]')
-    tryLink: By = By.xpath('//a[@href="http://www.testproject.io/"]')
-    sendFeedBackLink: By = By.xpath('//a[@href="http://gmail.com/"]')
-    willMumMakeMyBedBtnLink
-    : By = By.xpath('//a[@href="https://candymapper.com/mom-says-make-you-own-bed"]')
+export class Juans extends BasePage {
+    popUp: By = By.xpath('(//a[text() ="FIND MY CANDY!"])[4]')
+    demoLink: By = By.xpath('(//a[@class = "x-el x-el-a c1-3c c1-3d c1-3e c1-18 c1-19 c1-40 c1-s c1-b c1-8f c1-c c1-3m c1-a3 c1-a4 c1-d c1-e c1-f c1-g"])[6]')
+    moreBtn: By = By.xpath('//a[@class="x-el x-el-a c2-t c2-u c2-v c2-w c2-x c2-g c2-j c2-h c2-3 c2-y c2-z c2-10 c2-11 c2-12 c2-13 c2-14 c2-15 c2-16"]')
+    momBtn: By = By.xpath('(//a[text()= "Magic Object Model?"])[2]')
+    testProjectLink: By = By.xpath('//a[text()= "TestProject.io"]')
+    tryLink: By = By.xpath('(//a[@class="x-el x-el-a c1-3d c1-3e c1-8v c1-18 c1-19 c1-41 c1-s c1-75 c1-b c1-43 c1-3n c1-44 c1-3p"])[3]')
+    sendFeedBackLink: By = By.xpath('//a[text()= "TheDarkArtsWizard@gmail.com"]')
+    willMumMakeMyBedBtnLink: By = By.xpath('//a[text()= "Will Mum Make my Bed?"]')
 
 
 
@@ -19,5 +20,13 @@ export class candyMapPage extends BasePage {
         await this.driver.manage().window().maximize()
         await this.navigate(this.url)
         await this.click(this.popUp)
+    }
+    async tabs() {
+        let myTabs = await this.driver.getAllWindowHandles();
+        await this.driver.switchTo().window(myTabs[1]);
+        await this.driver.sleep(1500);
+        await this.driver.close();
+        await this.driver.switchTo().window(myTabs[0]);
+        //this method is for closing newly opened tabs
     }
 }
--- juansTest.test.ts
+++ juansTest.test.ts
@@ -1,24 +1,31 @@
 import {Driver} from "selenium-webdriver/chrome";
-import {juans} from "./juansPageObject";
+import {Juans} from "./juansPageObject";
 
-const candyMP = new candyMapPage();
+const juans = new Juans();
 
 test("Close that popup", async ()=> {
     await juans.closePop()
 })
 test("Go to demoLink", async ()=> {
     await juans.click(juans.demoLink)
+    await juans.tabs()
 })
 test("Go to testProjectLink", async ()=>{
+    await juans.click(juans.moreBtn)
+    await juans.click(juans.momBtn)
     await juans.click(juans.testProjectLink)
+    await juans.tabs()
 })
 test("Go to tryLink", async ()=> {
     await juans.click(juans.tryLink)
+    await juans.tabs()
 })
 test("Go to sendFeedBackLink", async ()=> {
     await juans.click(juans.sendFeedBackLink)
+    await juans.tabs()
 })
 test("Go to willMumMakeMyBedLink", async ()=> {
-    await juans.click(juans.willMumMakeMyBedLink)
-})
+    await juans.click(juans.willMumMakeMyBedBtnLink)
 
+    await juans.driver.quit()
+})
